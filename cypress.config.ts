2ccdb5515c64b7bc51eaeab425e68fe5e5b048e6
Christian Deme <christian.deme@cleanercode.de>
2023-08-31 10:32:14 +0200

    chore: set screenshotting to default value (true)

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,19 +1,14 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-
   component: {
     devServer: {
-      framework: 'next',
       bundler: 'webpack',
+      framework: 'next',
     },
   },
 
+  e2e: {},
+
   video: false,
-  screenshotOnRunFailure: false,
 })
