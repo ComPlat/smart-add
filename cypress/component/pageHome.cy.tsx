d08fecceb9777f886c5303ab354277ea034464f3
Christian Deme <christian.deme@cleanercode.de>
2023-09-05 09:54:27 +0200

    test: remove useless test

--- cypress/component/pageHome.cy.tsx
+++ cypress/component/pageHome.cy.tsx
@@ -1,10 +1,6 @@
 import Home from '../../src/app/page'
 
 describe('<Home />', () => {
-  it('renders', () => {
-    cy.mount(<Home />)
-  })
-
   it('has headline', () => {
     cy.mount(<Home />)
     cy.get('h1').contains('SmartAdd')
