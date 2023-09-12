111f622b3fb9b80808a736b2825450d533cf93eb
Christian Deme <christian.deme@cleanercode.de>
2023-09-12 14:56:28 +0200

    text: fix component test

--- cypress/component/pageHome.cy.tsx
+++ cypress/component/pageHome.cy.tsx
@@ -3,6 +3,6 @@ import Home from '../../src/app/page'
 describe('<Home />', () => {
   it('has headline', () => {
     cy.mount(<Home />)
-    cy.get('h1').contains('SmartAdd')
+    cy.get('h1').contains('Upload')
   })
 })
