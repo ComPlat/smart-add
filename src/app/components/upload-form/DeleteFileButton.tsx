692c809d7451f9f078c7d883a9e59ceec9035ac7
Christian Deme <christian.deme@cleanercode.de>
2023-09-21 16:37:06 +0200

    chore: extract onClick delete to its own function

--- src/app/components/upload-form/DeleteFileButton.tsx
+++ src/app/components/upload-form/DeleteFileButton.tsx
@@ -1,24 +1,29 @@
 import { ExtendedFile, filesDB } from '@/database/db'
 import { DeleteOutlined } from '@ant-design/icons'
 
+const handleDelete = (
+  file: ExtendedFile,
+  event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+) => {
+  event.preventDefault()
+
+  if (!('id' in file)) {
+    throw new TypeError('Can not delete because the file is missing an id!')
+  }
+
+  if (!(typeof file.id === 'number')) {
+    throw new TypeError(
+      'Can not delete because the id of file is not a number!',
+    )
+  }
+
+  filesDB.files.delete(file.id)
+}
+
 const DeleteFileButton = ({ file }: { file: ExtendedFile }) => (
   <button
-    onClick={(event) => {
-      event.preventDefault()
-
-      if (!('id' in file)) {
-        throw new TypeError('Can not delete because the file is missing an id!')
-      }
-
-      if (!(typeof file.id === 'number')) {
-        throw new TypeError(
-          'Can not delete because the id of file is not a number!',
-        )
-      }
-
-      filesDB.files.delete(file.id)
-    }}
     className="rounded-sm px-0.5 text-neutral-400 transition-all hover:border-neutral-200 hover:bg-neutral-200 hover:text-neutral-900"
+    onClick={(event) => handleDelete(file, event)}
   >
     <DeleteOutlined />
   </button>
