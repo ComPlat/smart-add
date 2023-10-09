582da988200f16bd0800a1e5e690815ee9b05706
Jan Hauck <jan.hauck@cleanercode.de>
2023-10-09 16:34:21 +0200

    wip: add xlsx parsing cypress test

---
+++ cypress/e2e/xlsxParsing.cy.ts
@@ -0,0 +1,30 @@
+import '@this-dot/cypress-indexeddb'
+
+describe('XLSX', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+  afterEach(() => {
+    cy.clearIndexedDb('filesDatabse')
+  })
+
+  describe('xlsx upload prep', () => {
+    const xlsxFileName = 'file_example_XLSX_50.xlsx'
+
+    beforeEach(() => {
+      cy.fixture(xlsxFileName).as('testFile')
+    })
+
+    it('xlsx file upload', () => {
+      cy.get('span[role=button]').selectFile('@testFile', {
+        action: 'drag-drop',
+      })
+      cy.wait(10_000)
+      cy.get('p').contains(xlsxFileName)
+
+      cy.get('input.w-full').type(xlsxFileName)
+      cy.get('button.rounded-md').click({ force: true }) // click doesn't work
+      cy.wait(1_000)
+    })
+  })
+})
