8bfe8ac93afbff65bdb139cb206de91bcfb1820e
Jan Hauck <jan.hauck@cleanercode.de>
2023-10-11 13:55:28 +0200

    Added separate excel upload input for testing

--- src/app/components/upload-form/ParseXlsx.tsx
+++ src/app/components/upload-form/ParseXlsx.tsx
@@ -5,6 +5,9 @@ import { useState } from 'react'
 import { JSONTree } from 'react-json-tree'
 import * as XLSX from 'xlsx'
 
+const EXCEL_FILE =
+  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+
 const ParseXlsx = () => {
   const [path, setPath] = useState('')
   const [output, setOutput] = useState(null as XLSX.WorkBook | null)
@@ -27,6 +30,23 @@ const ParseXlsx = () => {
     reader.readAsArrayBuffer(fileAsFile)
   }
 
+  const handleFileUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!(e.target instanceof HTMLInputElement)) return
+
+    if (!e.target.files) return
+
+    const reader = new FileReader()
+    reader.onload = (event) => {
+      const arrayBuffer = event.target?.result
+      if (!arrayBuffer) return
+
+      const workbook = XLSX.read(arrayBuffer, { type: 'binary' })
+      setOutput(workbook)
+    }
+
+    reader.readAsArrayBuffer(e.target.files[0])
+  }
+
   return (
     <div className="flex flex-col gap-4">
       <input
@@ -36,12 +56,20 @@ const ParseXlsx = () => {
         type="text"
         value={path}
       />
-      <button
-        className="rounded-md bg-black p-4 text-white hover:bg-gray-700"
-        onClick={handleFileChange}
-      >
-        Parse XLSX
-      </button>
+      <div className="flex flex-row gap-4">
+        <button
+          className="flex-1 rounded-md bg-black p-4 text-white hover:bg-gray-700"
+          onClick={handleFileChange}
+        >
+          Parse XLSX
+        </button>
+        <input
+          accept={EXCEL_FILE}
+          className="flex-1 rounded-md border border-black p-4 text-gray-700"
+          onChange={handleFileUpload}
+          type="file"
+        />
+      </div>
       {output && (
         <div>
           <h2>Output</h2>
