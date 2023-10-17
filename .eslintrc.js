51115a5587a6caf23388dacb063b50ee1ee6b4f8
Jan Hauck <jan.hauck@cleanercode.de>
2023-10-17 17:32:44 +0200

    fix: esling config fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,15 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'next/core-web-vitals',
   ],
+  overrides: [
+    {
+      files: ['cypress.config.ts', 'cypress/**/*.ts', 'cypress/**/*.js'],
+      rules: {
+        'promise/always-return': 'off',
+        'promise/catch-or-return': 'off',
+      },
+    },
+  ],
   parser: '@typescript-eslint/parser',
   plugins: ['prettier'],
   root: true,
@@ -14,19 +23,4 @@ module.exports = {
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
     'tailwindcss/classnames-order': 'error',
   },
-  // HINT: Disabled the following rules as they can't be caught or returned in Cypress
-  //       environment and cause false positives
-  overrides: [
-    {
-      extends: ['plugin:cypress/recommended'],
-      files: ['cypress.config.ts', 'cypress/**/*.ts', 'cypress/**/*.js'],
-      parserOptions: {
-        project: ['cypress/tsconfig.json'],
-      },
-      rules: {
-        'promise/always-return': 'off',
-        'promise/catch-or-return': 'off',
-      },
-    },
-  ],
 }
