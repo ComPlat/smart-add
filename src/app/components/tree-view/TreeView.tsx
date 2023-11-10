03c605e3ab2efda5cbfb5ba230f1106a05163f83
Jan Hauck <jan.hauck@cleanercode.de>
2023-11-10 15:51:51 +0100

    feat: improve tree view ux

--- src/app/components/tree-view/TreeView.tsx
+++ src/app/components/tree-view/TreeView.tsx
@@ -22,63 +22,53 @@ const TreeView = () => {
   const fileTree = constructTree(files)
 
   return (
-    <>
-      <style>{`
-        :root {
-          --rct-color-focustree-item-draggingover-bg: #ecdede;
-          --rct-color-focustree-item-draggingover-color: inherit;
-          --rct-color-search-highlight-bg: #7821e2;
-          --rct-color-drag-between-line-bg: rgb(37 99 235);
-        }
-      `}</style>
-      <UncontrolledTreeEnvironment
-        dataProvider={
-          new StaticTreeDataProvider(fileTree, (item, data) => ({
-            ...item,
-            data,
-          }))
-        }
-        canDragAndDrop
-        canDropOnFolder
-        canReorderItems
-        canSearch={false}
-        getItemTitle={(item) => item.data}
-        // HINT: Rerender when number of files changes
-        key={files.length}
-        viewState={{}}
-      >
-        <div className="flex flex-row justify-between">
-          <div style={{ width: '50%' }}>
-            <Tree
-              renderItemsContainer={({ children, containerProps }) => (
-                <ul {...containerProps}>{children}</ul>
-              )}
-              renderTreeContainer={({ children, containerProps }) => (
-                <div {...containerProps}>{children}</div>
-              )}
-              renderItem={renderItem}
-              rootItem="root"
-              treeId="tree-1"
-              treeLabel="Input Tree"
-            />
-          </div>
-          <div style={{ width: '50%' }}>
-            <Tree
-              renderItemsContainer={({ children, containerProps }) => (
-                <ul {...containerProps}>{children}</ul>
-              )}
-              renderTreeContainer={({ children, containerProps }) => (
-                <div {...containerProps}>{children}</div>
-              )}
-              renderItem={renderItem}
-              rootItem="root2"
-              treeId="tree-2"
-              treeLabel="Assignment Tree"
-            />
-          </div>
+    <UncontrolledTreeEnvironment
+      dataProvider={
+        new StaticTreeDataProvider(fileTree, (item, data) => ({
+          ...item,
+          data,
+        }))
+      }
+      canDragAndDrop
+      canDropOnFolder
+      canReorderItems
+      canSearch={false}
+      getItemTitle={(item) => item.data}
+      // HINT: Rerender when number of files changes
+      key={files.length}
+      viewState={{}}
+    >
+      <div className="flex flex-row justify-between">
+        <div style={{ width: '50%' }}>
+          <Tree
+            renderItemsContainer={({ children, containerProps }) => (
+              <ul {...containerProps}>{children}</ul>
+            )}
+            renderTreeContainer={({ children, containerProps }) => (
+              <div {...containerProps}>{children}</div>
+            )}
+            renderItem={renderItem}
+            rootItem="root"
+            treeId="tree-1"
+            treeLabel="Input Tree"
+          />
         </div>
-      </UncontrolledTreeEnvironment>
-    </>
+        <div style={{ width: '50%' }}>
+          <Tree
+            renderItemsContainer={({ children, containerProps }) => (
+              <ul {...containerProps}>{children}</ul>
+            )}
+            renderTreeContainer={({ children, containerProps }) => (
+              <div {...containerProps}>{children}</div>
+            )}
+            renderItem={renderItem}
+            rootItem="root2"
+            treeId="tree-2"
+            treeLabel="Assignment Tree"
+          />
+        </div>
+      </div>
+    </UncontrolledTreeEnvironment>
   )
 }
 
