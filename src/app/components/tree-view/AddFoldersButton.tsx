4583fc76dd269278fff4a0a7a514620b83621585
Jan Hauck <jan.hauck@cleanercode.de>
2023-11-24 12:50:33 +0100

    refactor: implement unique folder name function

--- src/app/components/tree-view/AddFoldersButton.tsx
+++ src/app/components/tree-view/AddFoldersButton.tsx
@@ -3,92 +3,76 @@ import { FileNode } from '@/helper/types'
 import { Button } from 'antd'
 import { v4 } from 'uuid'
 
-const clickHandler = async (tree: Record<string, FileNode>) => {
-  const baseFolderName1 = 'Samples'
-  let folderName1 = baseFolderName1
-  let counter1 = 1
+const generateUniqueFolderName = (
+  baseFolderName: string,
+  tree: Record<string, FileNode>,
+  counter = 0,
+): string => {
+  const folderName =
+    counter === 0 ? baseFolderName : `${baseFolderName}_${counter}`
 
-  while (tree[folderName1]) {
-    folderName1 = `${baseFolderName1}_${counter1}`
-    counter1++
-  }
+  return tree[folderName]
+    ? generateUniqueFolderName(baseFolderName, tree, counter + 1)
+    : folderName
+}
+
+const clickHandler = async (tree: Record<string, FileNode>) => {
+  const uniqueFolderName1 = generateUniqueFolderName('Samples', tree)
+
   const newFolderNode1 = {
     canMove: true,
     canRename: true,
     children: [],
-    data: folderName1,
-    index: folderName1,
+    data: uniqueFolderName1,
+    index: uniqueFolderName1,
     isFolder: true,
     uid: v4(),
   }
 
-  const baseFolderName2 = 'Reactions'
-  let folderName2 = baseFolderName2
-  let counter2 = 1
-
-  while (tree[folderName2]) {
-    folderName2 = `${baseFolderName2}_${counter2}`
-    counter2++
-  }
+  const uniqueFolderName2 = generateUniqueFolderName('Reactions', tree)
 
   const newFolderNode2 = {
     canMove: true,
     canRename: true,
     children: [],
-    data: folderName2,
-    index: folderName2,
+    data: uniqueFolderName2,
+    index: uniqueFolderName2,
     isFolder: true,
     uid: v4(),
   }
 
-  const baseFolderName3 = 'Analyses'
-  let folderName3 = baseFolderName3
-  let counter3 = 1
-
-  while (tree[folderName3]) {
-    folderName3 = `${baseFolderName3}_${counter3}`
-    counter3++
-  }
+  const uniqueFolderName3 = generateUniqueFolderName('Analyses', tree)
 
   const newFolderNode3 = {
     canMove: true,
     canRename: true,
     children: [],
-    data: folderName3,
-    index: folderName3,
+    data: uniqueFolderName3,
+    index: uniqueFolderName3,
     isFolder: true,
     uid: v4(),
   }
 
   await Promise.all([
     assignmentsDB.assignedFolders.add({
-      // extension: 'folder',
-      // file: new File([''], newFolderNode1.data),
       fullPath: newFolderNode1.index,
       isFolder: true,
       name: newFolderNode1.data,
       parentUid: v4(),
-      // path: [],
       uid: newFolderNode1.uid,
     }),
     assignmentsDB.assignedFolders.add({
-      // extension: 'folder',
-      // file: new File([''], newFolderNode2.data),
       fullPath: newFolderNode2.index,
       isFolder: true,
       name: newFolderNode2.data,
       parentUid: v4(),
-      // path: [],
       uid: newFolderNode2.uid,
     }),
     assignmentsDB.assignedFolders.add({
-      // extension: 'folder',
-      // file: new File([''], newFolderNode3.data),
       fullPath: newFolderNode3.index,
       isFolder: true,
       name: newFolderNode3.data,
       parentUid: v4(),
-      // path: [],
       uid: newFolderNode3.uid,
     }),
   ])
