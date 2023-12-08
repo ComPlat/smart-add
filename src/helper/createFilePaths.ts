390ecfb8a61b8c1f8382a12cc6b9ea50027c5285
Jan Hauck <jan.hauck@cleanercode.de>
2023-12-08 10:15:03 +0100

    refactor: move nested methods to top scope of file

--- src/helper/createFilePaths.ts
+++ src/helper/createFilePaths.ts
@@ -1,33 +1,32 @@
 import { FileNode } from './types'
 
+const getFullPath = (currentPath: string, nodeData: string, isRoot: boolean) =>
+  isRoot ? '' : `${currentPath}${currentPath ? '/' : ''}${nodeData}`
+
+const traverseTree = (
+  fileTree: Record<string, FileNode>,
+  node: FileNode,
+  currentPath: string,
+  isRoot: boolean,
+  filePaths: { name: string; path: string; uid: string }[],
+): Promise<void>[] => {
+  const fullPath = getFullPath(currentPath, node.data, isRoot)
+  filePaths.push({ name: node.data, path: fullPath, uid: node.uid! })
+
+  return (node.children?.map((child: string) =>
+    traverseTree(fileTree, fileTree[child], fullPath, false, filePaths),
+  )).flat()
+}
+
 const createFilePaths = (
   fileTree: Record<string, FileNode>,
   root: string,
 ): { name: string; path: string; uid: string }[] => {
   const filePaths: { name: string; path: string; uid: string }[] = []
 
-  const getFullPath = (
-    currentPath: string,
-    nodeData: string,
-    isRoot: boolean,
-  ) => (isRoot ? '' : `${currentPath}${currentPath ? '/' : ''}${nodeData}`)
-
-  const traverseTree = (
-    node: FileNode,
-    currentPath: string,
-    isRoot: boolean,
-  ): Promise<void>[] => {
-    const fullPath = getFullPath(currentPath, node.data, isRoot)
-    filePaths.push({ name: node.data, path: fullPath, uid: node.uid! })
-
-    return (node.children?.map((child: string) =>
-      traverseTree(fileTree[child], fullPath, false),
-    )).flat()
-  }
-
   const rootFolder = fileTree[root]
   if (rootFolder) {
-    traverseTree(rootFolder, '', true)
+    traverseTree(fileTree, rootFolder, '', true, filePaths)
   }
 
   return filePaths
