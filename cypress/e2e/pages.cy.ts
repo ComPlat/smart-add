5d2af1066d618f06f35a824e53029cfc9d41860d
Jan Hauck <jan.hauck@cleanercode.de>
2024-01-19 17:56:28 +0100

    fix: adjust cypress test

--- cypress/e2e/pages.cy.ts
+++ cypress/e2e/pages.cy.ts
@@ -2,37 +2,31 @@ import '@this-dot/cypress-indexeddb'
 
 describe('Pages', () => {
   beforeEach(() => {
-    cy.visit('/').wait(100).clearIndexedDb('filesDatabase')
+    cy.visit('/')
+    cy.clearIndexedDb('filesDatabase')
   })
 
   describe('UploadForm', () => {
     const fileName = 'file.json'
-
-    beforeEach(() => {
-      cy.fixture(fileName).as('testFile')
-    })
+    const fileFixturePath = `cypress/fixtures/${fileName}`
 
     context('when uploading per drag and drop', () => {
       beforeEach(() => {
-        cy.get('span[role=button]').selectFile('@testFile', {
+        cy.get('span[role=button]').selectFile(fileFixturePath, {
           action: 'drag-drop',
         })
       })
 
       it('shows message that upload was successful', () => {
-        cy.get('button').contains(fileName)
-
         cy.get('.ant-message-success').should(
           'contain',
           `${fileName} uploaded successfully`,
         )
+        cy.get('span').should('contain', fileName)
       })
 
       it('fills IndexedDB database', () => {
-        cy.openIndexedDb('filesDatabase')
-          .openIndexedDb('filesDatabase')
-          .createObjectStore('files')
-          .as('files')
+        cy.openIndexedDb('filesDatabase').createObjectStore('files').as('files')
 
         cy.getStore('@files').keys().should('not.be.empty')
       })
