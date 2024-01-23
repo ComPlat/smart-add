121f095371004eb9a94309c55be462c16150b51d
Christian Deme <christian.deme@cleanercode.de>
2024-01-23 16:49:23 +0100

    fix: add null to type

--- src/app/components/context-menu/ContextMenuItem.tsx
+++ src/app/components/context-menu/ContextMenuItem.tsx
@@ -1,7 +1,7 @@
 const ContextMenuItem = ({
   renderContextMenu,
 }: {
-  renderContextMenu: JSX.Element
+  renderContextMenu: JSX.Element | null
 }) => (
   <ul className="px-1 [&>li:hover]:bg-gray-300 [&>li]:my-1 [&>li]:cursor-pointer [&>li]:rounded [&>li]:px-2 [&>li]:py-1">
     {renderContextMenu}
