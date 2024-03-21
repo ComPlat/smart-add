9c0843436a43241e65239fdb34ecf45053d24f0b
Christian Deme <christian.deme@cleanercode.de>
2024-03-21 12:51:29 +0100

    feat: save files in attachments folder

--- src/app/components/zip-download/FileDownloader.tsx
+++ src/app/components/zip-download/FileDownloader.tsx
@@ -5,6 +5,7 @@ import { message } from 'antd'
 import { useLiveQuery } from 'dexie-react-hooks'
 import { saveAs } from 'file-saver'
 import JSZip from 'jszip'
+import { v4 } from 'uuid'
 
 import { Button } from '../workspace/Button'
 import { generateExportJson } from './jsonGenerator'
@@ -47,24 +48,23 @@ const FileDownloader = () => {
       const zip = new JSZip()
       const fileTree = constructTree(assignedFiles)
 
-      const addFilesToZip = async (
-        tree: FileTree,
-        path: string,
-        parentZip: JSZip = zip,
-      ) => {
+      const addFilesToZip = async (tree: FileTree, parentZip: JSZip = zip) => {
         await Promise.all(
           Object.entries(tree).map(async ([name, value]) => {
-            const newPath = path ? `${path}/${name}` : name
+            const extension = name.split('.').pop() // Get the file extension
+            const newFilename = `${v4()}.${extension}`
+
+            const newPath = `attachments/${newFilename}`
 
             if (name.endsWith('.zip') && typeof value === 'object') {
               const nestedZip = new JSZip()
-              await addFilesToZip(value, '', nestedZip)
+              await addFilesToZip(value, nestedZip)
               const nestedBlob = await nestedZip.generateAsync({ type: 'blob' })
               parentZip.file(newPath, nestedBlob)
             } else if (value instanceof Blob) {
               parentZip.file(newPath, value)
             } else {
-              await addFilesToZip(value, newPath, parentZip)
+              await addFilesToZip(value, parentZip)
             }
           }),
         )
@@ -72,7 +72,7 @@ const FileDownloader = () => {
         return parentZip
       }
 
-      await addFilesToZip(fileTree, '')
+      await addFilesToZip(fileTree)
 
       const exportJsonData = await generateExportJson(
         assignedFiles,
