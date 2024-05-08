ddf789b2af35f38a73e33693248eb6538d1e5681
Christian Deme <christian.deme@cleanercode.de>
2024-05-08 10:55:31 +0200

    refactor: change dropdown name to a more precise one

--- src/app/components/input-components/DropDownMenu.tsx
+++ src/app/components/input-components/DropDownMenu.tsx
@@ -4,10 +4,10 @@ const DropDownMenu = () => {
       className={`mt-2 rounded border bg-white px-2 py-1
         text-sm outline-gray-200
         hover:border-kit-primary-full focus:border-kit-primary-full`}
-      name="reaction-type"
+      name="reaction-scheme-type"
     >
       <option value="none">None</option>
-      <optgroup label="Reaction type">
+      <optgroup label="Reaction scheme type">
         <option value="starting-material">Starting material</option>
         <option value="reactant">Reactant</option>
         <option value="product">Product</option>
