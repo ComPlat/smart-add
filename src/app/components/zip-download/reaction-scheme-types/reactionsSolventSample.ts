4b92c332908bab2d8582c9a88875180ba4503b4c
Christian Deme <christian.deme@cleanercode.de>
2024-08-05 10:58:05 +0200

    style: remove unused code

--- src/app/components/zip-download/reaction-scheme-types/reactionsSolventSample.ts
+++ src/app/components/zip-download/reaction-scheme-types/reactionsSolventSample.ts
@@ -1,4 +1,3 @@
-import { ExtendedFolder } from '@/database/db'
 import { ReactionSchemeProps } from '@/types/ReactionSchemeProps'
 import { v4 } from 'uuid'
 
@@ -7,47 +6,6 @@ import {
   ReactionScheme as ReactionSchemeType,
   reactionSchemeSchema,
 } from '../zodSchemes'
-
-interface ReactionsSolventSampleParams {
-  processedFolders: ExtendedFolder[]
-  sampleReactionUidMap: Record<string, string>
-}
-
-// export const ReactionsSolventSample = ({
-//   processedFolders,
-//   sampleReactionUidMap,
-// }: ReactionsSolventSampleParams): Record<string, ReactionSchemeType> => {
-//   return processedFolders.reduce(
-//     (uidToReactionsSolventSample, folder) => {
-//       if (folder.dtype !== 'reaction') return uidToReactionsSolventSample
-
-//       const reactionId = sampleReactionUidMap[folder.uid]
-//       const childSamples = processedFolders.filter(
-//         (sampleFolder) =>
-//           sampleFolder.fullPath.startsWith(folder.fullPath + '/') &&
-//           sampleFolder.dtype === 'sample',
-//       )
-
-//       return childSamples.reduce(
-//         (innerUidToReactionsSolventSample, sampleFolder) => {
-//           const reactionsSolventSampleId = v4()
-//           const newReactionSolventSample = {
-//             ...reactionSchemeSchema.parse({
-//               ...reactionSchemeTemplate,
-//               reaction_id: reactionId,
-//               sample_id: sampleReactionUidMap[sampleFolder.uid],
-//             }),
-//           }
-//           innerUidToReactionsSolventSample[reactionsSolventSampleId] =
-//             newReactionSolventSample
-//           return innerUidToReactionsSolventSample
-//         },
-//         uidToReactionsSolventSample,
-//       )
-//     },
-//     {} as Record<string, ReactionSchemeType>,
-//   )
-// }
 
 export const ReactionsSolventSample = ({
   assignedFolders,
