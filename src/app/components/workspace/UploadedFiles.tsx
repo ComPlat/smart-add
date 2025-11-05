ed9396f34c5b9fa6a56d2a536a81e16febacd4a6
Ali Zaib <alizaib1217@gmail.com>
2025-11-05 10:53:51 +0100

    fix: added sample name to export.json

--- src/app/components/workspace/UploadedFiles.tsx
+++ src/app/components/workspace/UploadedFiles.tsx
@@ -9,7 +9,7 @@ const UploadedFiles = ({
     e: React.MouseEvent<HTMLDivElement, MouseEvent>,
   ) => Promise<void>
 }) => (
-  <div className="flex w-1/2 flex-col" onContextMenu={onContextMenu}>
+  <div className="flex w-full flex-col" onContextMenu={onContextMenu}>
     <div className="relative h-full overflow-y-auto bg-white px-2 py-4 shadow-sm">
       {children}
     </div>
