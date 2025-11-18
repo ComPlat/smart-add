3ffb1825970b8ef9ce5f95da5e3ff6ab0ec43e41
Ali Zaib <alizaib1217@gmail.com>
2025-11-18 15:14:16 +0100

    fixed ketcher issue to set empty structure

--- src/app/components/input-components/KetcherModal.tsx
+++ src/app/components/input-components/KetcherModal.tsx
@@ -39,7 +39,7 @@ export default function KetcherModal({
           console.error('Error loading molfile:', error)
         })
       } else {
-        ketcher.setMolecule("").catch((error: Error) => {
+        ketcher.setMolecule('').catch((error: Error) => {
           console.error('Error clearing editor:', error)
         })
       }
