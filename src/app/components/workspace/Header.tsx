107adb68169a446e41680d5dfdd6564ecd45c9e0
Ali Zaib <alizaib1217@gmail.com>
2025-12-03 14:21:05 +0100

    fixed logo image loading issue

--- src/app/components/workspace/Header.tsx
+++ src/app/components/workspace/Header.tsx
@@ -1,19 +1,23 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import chemotionLogo from '../../../../public/Chemotion_full.svg'
-import logo from '../../../../public/logo.png'
 
 const Header = () => {
   return (
     <header className="flex w-full flex-row justify-between bg-white px-4 py-2 shadow-sm h-24 items-center">
       <Link href="https://chemotion.net/" className="self-center">
-        <Image alt="Chemotion Logo" src={chemotionLogo} />
+        <Image
+          alt="Chemotion Logo"
+          width={100}
+          height={60}
+          src="/Chemotion_full.svg"
+        />
       </Link>
       <Image
         alt="SmartAdd Logo"
         className="self-center"
         width={100}
-        src={logo}
+        height={60}
+        src="/logo.png"
       />
       <Link
         href="https://chemotion.net/docs/services/smartadd"
