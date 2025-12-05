e82a598028bca2be18de880fecf1330ddce739fa
Ali Zaib <45627267+alizaib1217@users.noreply.github.com>
2025-12-05 10:10:19 +0100

    fixed logo image loading issue (#47)

--- src/app/components/workspace/Header.tsx
+++ src/app/components/workspace/Header.tsx
@@ -1,7 +1,5 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import chemotionLogo from '../../../../public/Chemotion_full.svg'
-import logo from '../../../../public/logo.png'
 import { Badge, Popover } from 'antd'
 import { useState } from 'react'
 import { FcSettings } from 'react-icons/fc'
@@ -19,13 +17,19 @@ const Header = () => {
   return (
     <header className="flex w-full flex-row justify-between bg-white px-4 py-2 shadow-sm h-24 items-center">
       <Link href="https://chemotion.net/" className="self-center">
-        <Image alt="Chemotion Logo" src={chemotionLogo} />
+        <Image
+          alt="Chemotion Logo"
+          width={100}
+          height={60}
+          src="/Chemotion_full.svg"
+        />
       </Link>
       <Image
         alt="SmartAdd Logo"
         className="self-center"
         width={100}
-        src={logo}
+        height={60}
+        src="/logo.png"
       />
       <Popover
         content={
